ff04895415fc62ba6648f46e5e353da115cf8b29
Martijn Eerens <martijn@eerens.nl>
2018-06-27 15:45:09 +0200

    add fields to meta config

--- meta.js
+++ meta.js
@@ -1,25 +1,49 @@
 module.exports = {
-  helpers: {
-    escape: function(value) {
-      return value.replace(/'/g, '&apos;');
-    }
-  },
-  prompts: {
-    name: {
-      'type': 'string',
-      'required': true,
-      'message': 'Project name'
+    helpers: {
+        escape: function (value) {
+            return value.replace(/'/g, '&apos;');
+        }
     },
-    description: {
-      'type': 'string',
-      'required': false,
-      'message': 'Project description',
-      'default': 'Nuxt.js project'
+    prompts: {
+        name: {
+            'type': 'string',
+            'required': true,
+            'message': 'Project name'
+        },
+        title: {
+            'type': 'string',
+            'required': true,
+            'message': 'Project title',
+            'default': 'De titel van een productie'
+        },
+        description: {
+            'type': 'string',
+            'required': true,
+            'message': 'Project description',
+            'default': 'Dit is een special over x.'
+        },
+        year: {
+            'type': 'string',
+            'required': false,
+            'message': 'year',
+            'default': (new Date()).getFullYear()
+        },
+        directus_table: {
+            'type': 'string',
+            'required': false,
+            'message': 'Directus content table name',
+            'default': 'producties'
+        },
+        directus_project_id: {
+            'type': 'int',
+            'required': false,
+            'message': 'Directus project id',
+            'default': 1
+        },
+        author: {
+            'type': 'string',
+            'message': 'Author'
+        },
     },
-    author: {
-      'type': 'string',
-      'message': 'Author'
-    },
-  },
-  completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
+    completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
 };
