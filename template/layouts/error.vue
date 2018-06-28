f94463d908de13637d1632f6459ff716e2da2041
Martijn Eerens <martijn@eerens.nl>
2018-06-28 11:29:22 +0200

    remove template strings

--- template/layouts/error.vue
+++ template/layouts/error.vue
@@ -3,7 +3,7 @@
         <h1 v-if="error.statusCode === 404">Deze pagina kan niet worden gevonden</h1>
         <div v-else>
             <h1>Er is een probleem opgetreden:</h1>
-            <h3>{{error.message}}</h3>
+            <h3 v-html="error.message"></h3>
         </div>
         <nuxt-link to="/">Ga terug naar de homepage</nuxt-link>
     </div>
