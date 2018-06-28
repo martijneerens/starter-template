79403e8ef8a7564c76241cc9757efdc5fde855f2
Martijn Eerens <martijn@eerens.nl>
2018-06-28 11:32:36 +0200

    add header to error page

--- template/layouts/error.vue
+++ template/layouts/error.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <vk-header></vk-header>
         <h1 v-if="error.statusCode === 404">Deze pagina kan niet worden gevonden</h1>
         <div v-else>
             <h1>Er is een probleem opgetreden:</h1>
@@ -10,8 +11,13 @@
 </template>
 
 <script>
+    import VkHeader from '~/components/Header.vue';
+
     export default {
         props: ['error'],
-        layout: 'defafult'
+        layout: 'defafult',
+        components: {
+            VkHeader
+        },
     }
 </script>
