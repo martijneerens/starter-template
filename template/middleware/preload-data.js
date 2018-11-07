0104011350f212729ea9108c9c2716e6a04a30d7
Martijn Eerens <martijn@eerens.nl>
2018-11-07 10:27:00 +0100

    fetch data from live cms api

--- template/middleware/preload-data.js
+++ template/middleware/preload-data.js
@@ -1,8 +1,26 @@
 //fetch data from cms on load
 
+let checkInterval;
+
 export default function ({ store }) {
+
     if (store.state.items.length === 0) {
-        return store.dispatch('setData');
+        if(store.state.useDirectus) {
+            return new Promise(function(resolve) {
+                store.dispatch('setData');
+                checkInterval = setInterval(()=>{
+                    if(store.state.dataLoaded){
+                        clearInterval(checkInterval);
+                        resolve();
+                    }
+                }, 100)
+            });
+        }
+        else{
+            return store.dispatch('setData');
+        }
     }
-    return true;
+    else{
+        return true;
+    }
 }
--- template/store/index.js
+++ template/store/index.js
@@ -27,9 +27,49 @@ const createStore = () => {
             },
 
             async setData({commit}) {
-                const data = await this.$axios.get(`data/data.json`);
-                commit('setItems', data.data.{{ directus_table }});
-                commit('setMeta', data.data.meta);
+                let useDirectus = this.state.useDirectus;
+
+                if (!useDirectus) {
+                    const data = await this.$axios.get(`data/data.json`);
+
+                    commit('setItems', data.data.{{ directus_table }});
+                    commit('setMeta', data.data.meta);
+                    commit('setLoaded', true);
+                }
+                else {
+                    console.log('loading live data from Directus');
+
+                    await require(['~/utils/directus'], (directus) => {
+
+                        let client = directus.client;
+                        let items = process.env.items;
+                        let params = {
+                            depth: 10
+                        };
+
+                        items.forEach(item => {
+                            if (item.id && item.api) {
+                                console.log(`getItem /${item.api}/${item.id} as ${item.name}`);
+
+                                client.getItem(item.api, item.id, params)
+                                    .then(res => {
+                                        commit('setMeta', res.data);
+                                    })
+                                    .catch(err => console.log(err));
+                            }
+                            else {
+                                console.log(`getItems /${item}`);
+
+                                client.getItems(item, params)
+                                    .then(res => {
+                                        commit('setItems', res.data);
+                                        commit('setLoaded', true);
+                                    })
+                                    .catch(err => console.log(err));
+                            }
+                        });
+                    });
+                }
             }
         },
 
